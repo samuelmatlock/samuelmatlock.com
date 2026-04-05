6998457e1c6d69544a4e3ca4599451f468c27257
MatlockS <samuel.matlock1@gmail.com>
2026-04-05 14:56:24 -0400

    Icon Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/smIcon.ico" />
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-M61FCSRJR9"
           strategy="afterInteractive"
